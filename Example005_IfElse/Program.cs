c36df8e19106560bf5fdc842f8ba9dd6b4134743
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-20 21:23:21 +0300

    dd ifelse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовтеля:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мaша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
